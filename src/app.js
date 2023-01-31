689aca4c9e1706801c2d3c121d8e3a44c6bcc664
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-31 15:16:45 -0300

    feat: connect with database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
-import express, { application } from "express";
+import express from "express";
 import dotenv from 'dotenv'
+import cors from 'cors'
+import { MongoClient } from "mongodb";
 
 dotenv.config()
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+try {
+  await mongoClient.connect()
+  db = mongoClient.db()
+  console.log("deu bom")
+} catch (error) {
+  console.error(error)
+}
+
 const app = express()
+app.use(cors())
+app.use(express.json())
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
