69f3267896c83f982c0d2e4eefd69a183a459fd9
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-31 16:46:57 -0300

    feat: update postChoice

--- src/app.js
+++ src/app.js
@@ -1,19 +1,14 @@
 import express from "express";
 import dotenv from 'dotenv'
 import cors from 'cors'
-
 import pool from "./route/poll.route.js";
 import choice from "./route/choice.route.js";
 
-
 dotenv.config()
-
 const app = express()
 app.use(cors())
 app.use(express.json())
-
 app.use([pool, choice])
-
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
--- src/controller/choice.controller.js
+++ src/controller/choice.controller.js
@@ -1,16 +1,28 @@
 import { ObjectId } from 'mongodb';
 import db from './../database/db.js';
+import { choiceSchema } from '../schema/choice.schema.js';
 
-export async function postChoice(req,res){
-  const  {title, pollId} = req.body
+export async function postChoice(req, res) {
+  const { title, pollId } = req.body
   try {
-    const poll = await db.collection('polls').findOne({_id:ObjectId(pollId)}) 
-    console.log(poll)
-    if(!poll) return res.sendStatus(404)
+    const { error } = choiceSchema.validate({
+      title,
+      pollId
+    }, { abortEarly: false });
+    if (error)
+      return res
+        .status(422)
+        .send(error.details.map((detail) => detail.message));
+    const poll = await db.collection('polls').findOne({ _id: ObjectId(pollId) })
+    if (!poll) return res.sendStatus(404)
 
-    const titlePoll = await db.collection('polls').findOne({title}) 
-    console.log(titlePoll)
-    if(!titlePoll) return res.sendStatus(409)
+
+    const titlePoll = await db.collection('choices').findOne({ title })
+    if (titlePoll) return res.sendStatus(409)
+    await db.collection('choices').insertOne({
+      title,
+      pollId: ObjectId(pollId)
+    })
     res.sendStatus(201)
   } catch (error) {
     return res.sendStatus(500)
