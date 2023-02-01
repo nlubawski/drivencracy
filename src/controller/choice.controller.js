e99fd6efca33b0b3ffbc12f794f93e8e0571740a
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-02-01 15:38:24 -0300

    feat: create postChoiceById

--- src/controller/choice.controller.js
+++ src/controller/choice.controller.js
@@ -36,3 +36,22 @@ export async function postChoice(req, res) {
   //ver se title é repetido 
   //ver se ja expirou
 }
+
+export async function postChoiceById(req, res) {
+  const choiceId  = req.params.id
+  try {
+    const choice = await db.collection('choices').findOne({_id:ObjectId(choiceId)})
+    if(!choice) return res.sendStatus(404)
+
+    const poll = await db.collection("polls").findOne({ _id: ObjectId(choice.pollId)});
+    if (!poll) return res.sendStatus(404)
+
+    const expired = dayjs().isAfter(poll.expireAt, 'days');
+    if (expired) return res.sendStatus(403)
+
+    await db.collection("votes").insertOne({choiceId, createdAt: dayjs().format('YYYY-MM-DD HH:mm')})
+    return res.sendStatus(201)
+  } catch (error) {
+    return res.sendStatus(500)
+  }
+}
--- src/route/choice.route.js
+++ src/route/choice.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { postChoice } from "../controller/choice.controller.js";
+import { postChoice, postChoiceById} from "../controller/choice.controller.js";
 
 const choice = Router()
 
 choice.post('/choice', postChoice)
+choice.post('/choice/:id/vote', postChoiceById)
 
 export default choice
